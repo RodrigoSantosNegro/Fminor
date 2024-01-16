df3e9fe396794e28fa6f70e5f214505f732a6cf1
Rodrigo <jsantosn23@fpcoruna.afundacion.org>
2024-01-16 12:29:53 +0100

    Base files added

---
+++ app/src/main/java/com/example/aptmc/MainTest.java
@@ -0,0 +1,16 @@
+package com.example.aptmc;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintest);
+    }
+
+}
