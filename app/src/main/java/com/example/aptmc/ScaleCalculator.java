080b9445dda70ad18a63ea93711bf996a77be168
Rodrigo <jsantosn23@fpcoruna.afundacion.org>
2024-01-24 10:50:17 +0100

    Scale Calculator and 50% of scale name added

--- app/src/main/java/com/example/aptmc/ScaleCalculator.java
+++ app/src/main/java/com/example/aptmc/ScaleCalculator.java
@@ -1,15 +1,21 @@
 package com.example.aptmc;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 
+
 public class ScaleCalculator extends AppCompatActivity{
+
+    private Button buttonScaleCalculator;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,10 +30,28 @@ public class ScaleCalculator extends AppCompatActivity{
         ArrayAdapter<CharSequence> scaleTypeAdapter=ArrayAdapter.createFromResource(this, R.array.scaleType_listBox, android.R.layout.simple_gallery_item);
         scaleTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 
-
         rootListBox.setAdapter(rootAdapter);
         scaleTypeListBox.setAdapter(scaleTypeAdapter);
+
+        buttonScaleCalculator = findViewById(R.id.buttonScaleCalculator);
+
+        buttonScaleCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String selectedRoot = rootListBox.getSelectedItem().toString();
+                String selectedScaleType = scaleTypeListBox.getSelectedItem().toString();
+                if(selectedScaleType.equals("Select scale type") || selectedRoot.equals("Select root")){
+                    Toast.makeText(ScaleCalculator.this, "You must select a Scale and the Root", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(ScaleCalculator.this, "Calculating scale... Root: " + selectedRoot + ", Scale Type: " + selectedScaleType, Toast.LENGTH_SHORT).show();
+                    // Send information to ScaleName
+                    Intent intent = new Intent(ScaleCalculator.this, ScaleName.class);
+                    intent.putExtra("ROOT_SELECTION", selectedRoot);
+                    intent.putExtra("SCALE_TYPE_SELECTION", selectedScaleType);
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
-
-
 }
