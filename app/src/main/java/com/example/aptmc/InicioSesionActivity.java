082463f81d7490cd80879401839fdfdc9d793915
Manstor21 <132900613+Manstor21@users.noreply.github.com>
2024-02-15 10:23:29 +0100

    loginSettingRegister

--- app/src/main/java/com/example/aptmc/InicioSesionActivity.java
+++ app/src/main/java/com/example/aptmc/InicioSesionActivity.java
@@ -3,6 +3,7 @@ package com.example.aptmc;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -34,11 +35,21 @@ public class InicioSesionActivity extends AppCompatActivity {
         editTextPassword = findViewById(R.id.editTextPasswordLogin);
 
         Button buttonLogin = findViewById(R.id.buttonLogin);
-        buttonLogin.setOnClickListener(v -> iniciarSesion());
-
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarSesion();
+            }
+        });
         // Agregar OnClickListener al botón de registro
         Button buttonRegister = findViewById(R.id.buttonRegister);
-        buttonRegister.setOnClickListener(v -> abrirActividadRegistro());
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirActividadRegistro();
+            }
+        });
+
     }
 
     @Override
